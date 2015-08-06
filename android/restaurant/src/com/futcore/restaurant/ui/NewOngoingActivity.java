781b3ea7bec473131664198bc36b67148023adc9
yanjzhou <yanjzhou@ebay.com>
2015-08-06 18:13:10 +0800

    up

--- android/restaurant/src/com/futcore/restaurant/ui/NewOngoingActivity.java
+++ android/restaurant/src/com/futcore/restaurant/ui/NewOngoingActivity.java
@@ -79,200 +79,37 @@ import com.futcore.restaurant.util.TimerFormat;
 
 //import android.support.v13.app.FragmentStatePagerAdapter;
 
-public class NewOngoingActivity extends WPActionBarActivity implements OnClickListener, LocationListener
+public class NewOngoingActivity extends WPActionBarActivity implements OnClickListener
 {
-    private Button cancelBut;
     private Button saveBut;
     private EditText nameEdit;
-    private EditText priceEdit;
 
-    private Location mCurrentLocation;
-    private LocationHelper mLocationHelper;
-
-    NotificationCompat.Builder mBuilder = null;
-    NotificationManager mNotifyMgr = null;
-
-    public static final int NOTI_DANGER = 1;
-    public static final int NOTI_ERGENT = 2;
-    public static final int NOTI_NORMAL = 3;
-    public static final int NOTI_LESS_IMPORTANT = 4;
-
+    private int ACTIVITY_NEW_EVENT = 0;
     
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_ongoing);
 
-        cancelBut = (Button)findViewById(R.id.cancelItem);
-        saveBut = (Button)findViewById(R.id.saveItem);
         nameEdit = (EditText)findViewById(R.id.itemname);
-        priceEdit = (EditText)findViewById(R.id.itemprice);
-
-        cancelBut.setOnClickListener(this);
+        saveBut = (Button)findViewById(R.id.saveWish);
+        
         saveBut.setOnClickListener(this);
-
-        getLocationProvider();
-
-        initNotification();
     }
 
-    private void initNotification()
-    {
-        Intent callIntent = new Intent(Intent.ACTION_CALL);
-        callIntent.setData(Uri.parse("tel:18516211115"));
-
-        PendingIntent pendingIntent0 = PendingIntent.getActivity(this, 0, callIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        
-        mBuilder = new NotificationCompat.Builder(this)
-            .setSmallIcon(R.drawable.ic_menu_view)
-            .setVibrate(new long[] { 1000, 1000, 1000})
-            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.spider_420))
-            //            .setUsesChronometer(true)
-            .setSubText("6 m")
-            //            .setWhen(System.currentTimeMillis())
-            .addAction(R.drawable.ic_cc_alarm, getString(R.string.event_finish), pendingIntent0)
-            ;
-
-        mNotifyMgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-    }
-
-    private void sendNotification(String title, String conStr, int type)
-    {
-        int mNotificationId = 001;
-        //        mNotifyMgr.notify(mNotificationId, mBuilder.build());
-        
-        switch(type){
-        case NOTI_DANGER:
-            mBuilder.setVibrate(new long[] { 1000, 2000, 1000});
-            break;
-        case NOTI_ERGENT:
-            mBuilder.setVibrate(new long[] { 1000, 1000, 1000});
-            break;
-        case NOTI_NORMAL:
-            mBuilder.setVibrate(new long[] { 1000, 500, 1000});
-            break;
-        case NOTI_LESS_IMPORTANT:
-            mBuilder.setVibrate(new long[] { 1000, 300, 1000});
-            break;
-        default:
-            ;
-        }
-        //    public static final int NOTI_DANGER = 1;
-        //    public static final int NOTI_ERGENT = 2;
-        //    public static final int NOTI_NORMAL = 3;
-        //    public static final int NOTI_LESS_IMPORTANT = 4;
-        
-        mBuilder.setContentTitle(title).
-            setContentText(conStr);
-        mNotifyMgr.notify(mNotificationId, mBuilder.build());        
-    }
-
-    private void sendNotification(String title, long duration, int type)
-    {
-        int mNotificationId = 001;
-        
-        switch(type){
-        case NOTI_DANGER:
-            mBuilder.setVibrate(new long[] { 1000, 2000, 1000});
-            break;
-        case NOTI_ERGENT:
-            mBuilder.setVibrate(new long[] { 1000, 1000, 1000});
-            break;
-        case NOTI_NORMAL:
-            mBuilder.setVibrate(new long[] { 1000, 500, 1000});
-            break;
-        case NOTI_LESS_IMPORTANT:
-            mBuilder.setVibrate(new long[] { 1000, 300, 1000});
-            break;
-        default:
-            ;
-        }
-        
-        mBuilder.setContentTitle(title).
-            setContentText("6 m");
-            //            .setWhen(System.currentTimeMillis() + duration);
-        
-        mNotifyMgr.notify(mNotificationId, mBuilder.build());        
-    }
-
-
-    private void getLocationProvider() {
-        boolean hasLocationProvider = false;
-        //        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        List<String> providers = locationManager.getProviders(true);
-
-        System.out.println("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz---------------------------------------------------------------1111111111111111111");
-        for (String providerName : providers) {
-            if (providerName.equals(LocationManager.GPS_PROVIDER) || providerName.equals(LocationManager.NETWORK_PROVIDER)) {
-                hasLocationProvider = true;
-            }
-        }
-        if (hasLocationProvider) {
-            System.out.println("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz---------------------------------------------------------------222222222222222222222222222");
-            //            System.out.println("hashashashashas");
-            enableLBSButtons();
-        }
-    }
-
-    private void enableLBSButtons()
-    {
-        mLocationHelper = new LocationHelper();
-        //        mLocationHelper.getLocation(ShakeShopActivity.this, locationResult);
-        System.out.println("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz---------------------------------------------------------------888888888888888888888888888888888");
-        mLocationHelper.getLocation(NewOngoingActivity.this, locationResult);
-    }
-
-    private LocationResult locationResult = new LocationResult() {
-            @Override
-            public void gotLocation(Location location) {
-                if (location != null) {
-                    System.out.println("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz---------------------------------------------------------------");
-                    mCurrentLocation = location;
-                } else {
-                    System.out.println("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz---------------------------------------------------------------7777777777777777777777777777777777777777");
-                    NewOngoingActivity.this.runOnUiThread(new Runnable() {
-                            public void run() {
-                            }
-                        });
-                }
-            }
-        };
-    
     public void onClick(View v)
     {
         int id = v.getId();
         switch(id){
-        case R.id.saveItem:
+        case R.id.saveWish:
             {
-                mLocationHelper.getLocation(NewOngoingActivity.this, locationResult);
-                AlertUtil.showAlert(NewOngoingActivity.this, R.string.required_fields, String.valueOf(mCurrentLocation.getLatitude())+" "+String.valueOf(mCurrentLocation.getLongitude())+" acc:"+String.valueOf(mCurrentLocation.getAccuracy()));
-                finish();
-            }
-            break;
-        case R.id.cancelItem:
-            {
-                //                sendNotification("test item", 2000, NOTI_NORMAL);
-                sendNotification("Free peach!", 2000, NOTI_NORMAL);
-                //                finish();
+                Intent intent = new Intent(Intent.ACTION_CALL);
+
+                intent.setData(Uri.parse("tel:18516211115"));
+                startActivity(intent);                
+                AlertUtil.showAlert(this, R.string.required_fields, nameEdit.getText().toString().trim());
             }
             break;
         }
-    }
-
-    public void onProviderDisabled(String provider) {
-    }
-
-    public void onProviderEnabled(String provider) {
-    }
-
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-    }
-
-    public void onLocationChanged(Location location) {
-        //        AlertUtil.showAlert(ShakeShopActivity.this, R.string.required_fields, R.string.url_username_password_required);
-        AlertUtil.showAlert(NewOngoingActivity.this, R.string.required_fields, R.string.url_username_password_required);
-        //                longitude = location.getLongitude();
-        //                latitude = location.getLatitude();
     }
 }
